e245b4aaf147eb2c7267603e756aa57f5654a419
swapi06 <swapimamuni@gmail.com>
2024-06-21 12:58:00 +0530

    idk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,14 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
+    <>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
     </Routes>
+    
     </BrowserRouter>
+    </>
   );
 }
 
