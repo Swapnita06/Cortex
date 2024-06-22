e86052efcc84ed1071d8e7a3584dc8fbfadeca94
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-06-22 16:14:59 +0530

    few more changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home';
 import Chat from './pages/Chat';
 import GroupChat from './pages/GroupChat';
+import Temp from './pages/Temp';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path="/chat" element={<Temp />} />
       <Route path="/chat/:modelName" element={<Chat />} />
       <Route path="/group-chat/:modelNames" element={<GroupChat />} /> {/* Add the GroupChat route */}
     </Routes>
