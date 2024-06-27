eb87c8087c77b371c68108c80b1bbb8e1a34ef88
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-06-27 16:47:19 +0530

    more updates

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import './Profile.css';
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
 
   return (
-    <div style={{ padding: '20px', color: 'white', fontFamily: 'Manrope' }}>
+    <div className='profile-container' style={{ padding: '20px', color: 'white', fontFamily: 'Manrope' }}>
       {isAuthenticated && (
         <div>
           <h3 className="user-name" style={{ fontWeight: '400', textAlign: 'center' }}>
@@ -18,11 +19,6 @@ const Profile = () => {
             <p style={{ fontWeight: '300' }}>Name: {user.name}</p>
             <p style={{ fontWeight: '300' }}>Nickname: {user.nickname}</p>
             <p style={{ fontWeight: '300' }}>Email: {user.email}</p>
-            <p style={{ fontWeight: '300' }}>Email Verified: {user.email_verified ? 'Yes' : 'No'}</p>
-            <p style={{ fontWeight: '300' }}>Updated At: {new Date(user.updated_at).toLocaleString()}</p>
-            {user.given_name && <p style={{ fontWeight: '300' }}>Given Name: {user.given_name}</p>}
-            {user.family_name && <p style={{ fontWeight: '300' }}>Family Name: {user.family_name}</p>}
-            {user.locale && <p style={{ fontWeight: '300' }}>Locale: {user.locale}</p>}
           </div>
         </div>
       )}
